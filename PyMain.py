e4f9ae7f5ee1be556a89a30b2bb0f343d5aad063
vladoshub <38497674+vladoshub@users.noreply.github.com>
2020-01-29 15:58:44 +0300

    Update PyMain.py

--- PyMain.py
+++ PyMain.py
@@ -1,7 +1,7 @@
-from subprocess import PIPE,Popen
+import subprocess
+from subprocess import Popen, PIPE
 
-caseOut = 
-{
+caseOut={
   'N': 'N-Текущая координата',
   'W': '2-Сделать измерение маха',
   'M': '3-получить измерение маха',
@@ -10,25 +10,25 @@ caseOut =
 
 def func(arg):
   if arg=='N':
-    print(p.stdin.readline().strip())
+        print(p.stdin.readline().strip())
   if arg=='M':
-    int k =int(p.stdin.readline().strip())
-    time = []
-    coordinate = []
-    while k > 0:
-      coordinate.append(int(p.stdin.readline().strip()))
-      time.append(float(p.stdin.readline().strip()))
-      k=k-1
-    return coordinate,time
+        k = int(p.stdin.readline().strip())
+        time = []
+        coordinate = []
+        while k > 0:
+             coordinate.append(int(p.stdin.readline().strip()))
+             time.append(float(p.stdin.readline().strip()))
+             k=k-1
+        return coordinate,time
 
 global p
 p = subprocess.Popen(["CRead","Read.C"], stdout=PIPE, stdin=PIPE)
 print('N:-Текущая координата,W-Сделать измерение маха,M-получить измерение маха,C-обнулить данные')
 value = input()
 while not (value in caseOut):
-  print('такой операции нет,попробуйте еще раз')
-  print('N:-Текущая координата,W-Сделать измерение маха,M-получить измерение маха,C-обнулить данные')
-  value = input()
+      print('такой операции нет,попробуйте еще раз')
+      print('N:-Текущая координата,W-Сделать измерение маха,M-получить измерение маха,C-обнулить данные')
+      value = input()
 print(caseOut[value])
 p.stdout.write(bytes(value + '\n', 'UTF-8'))
 func(value)

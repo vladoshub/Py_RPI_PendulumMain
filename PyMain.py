00cc83ce4a3bea2addabba4a4854a08bc4caa7a6
vladoshub <38497674+vladoshub@users.noreply.github.com>
2020-01-29 15:07:30 +0300

    Update PyMain.py

--- PyMain.py
+++ PyMain.py
@@ -1,4 +1,5 @@
-from subprocess import Popen, PIPE
+from subprocess import PIPE
+import sys
 
 caseOut = {
 'N': 'N-Текущая координата',
@@ -9,22 +10,21 @@ caseOut = {
 
 def func(arg):
 if arg=='N':
-print(p.stdout.readline().strip())
+print(sys.stdin.readline().strip())
 if arg=='M':
-int k =int(p.stdout.readline().strip())
-p.stdout.flush()
+int k =int(sys.stdin.readline().strip())
 time = []
 coordinate = []
 while k > 0:
-coordinate.append(int(p.stdout.readline().strip()))
-p.stdout.flush()
-time.append(float(p.stdout.readline().strip()))
-p.stdout.flush()
+coordinate.append(int(sys.stdin.readline().strip()))
+
+time.append(float(sys.stdin.readline().strip()))
+
 k=k-1
 return coordinate,time
 
 global p
-p = Popen(['bin:/Dekstop/Pendulum/Read.C'], shell=True, stdout=PIPE, stdin=PIPE)
+p = subprocess.run(['bin:/Dekstop/Pendulum/Read.C'], shell=True, stdout=PIPE, stdin=PIPE)
 print('N:-Текущая координата,W-Сделать измерение маха,M-получить измерение маха,C-обнулить данные')
 value = input()
 while not (value in caseOut):
@@ -32,6 +32,5 @@ print('такой операции нет,попробуйте еще раз')
 print('N:-Текущая координата,W-Сделать измерение маха,M-получить измерение маха,C-обнулить данные')
 value = input()
 print(caseOut[value])
-p.stdin.write(bytes(value + '\n', 'UTF-8'))
-p.stdin.flush()
+sys.stdout.write(bytes(value + '\n', 'UTF-8'))
 func(value)

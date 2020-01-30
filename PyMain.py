9fda877d59d366f7314e823fa2f01b38e49b1e21
vladoshub <38497674+vladoshub@users.noreply.github.com>
2020-01-30 10:13:18 +0300

    Update PyMain.py

--- PyMain.py
+++ PyMain.py
@@ -1,26 +1,55 @@
 import subprocess
 from subprocess import Popen, PIPE
 
-global p
-global init
+global p 
+global init 
 
-caseOut={
+Ops={
   'N': 'N-Текущая координата',
   'W': 'W-Сделать измерение маха',
   'M': 'M-получить измерение маха',
   'C': 'C-обнулить данные'
+  'S': 'S-изменить пороги чувствительнсои.1-MAX Отклонение от старта,2-MAX Отклонение от того на сколько маятник может отклоняться назад(вперед) при измерении'
 }
+
+def doChangeSensor(arg1,arg2):
+    try:
+        p.stdin.write(bytes('S' + '\n', 'UTF-8'))
+        p.stdin.write(bytes(arg1 + '\n', 'UTF-8'))
+        p.stdin.write(bytes(arg2 + '\n', 'UTF-8'))
+    except:
+        raise Exception('error')
+
+
+def doClear():
+    try:
+        p.stdin.write(bytes('С' + '\n', 'UTF-8'))
+    except:
+        raise Exception('error')
+
+
+
+
+def doMeasurement():
+    try:
+        p.stdin.write(bytes('W' + '\n', 'UTF-8'))
+    except:
+        raise Exception('error')
+
+
 
 def getDataCoordinate():
     try:
+        p.stdin.write(bytes('N' + '\n', 'UTF-8'))
         l = int(p.stdin.readline().strip())
         return l
     except:
         raise Exception('error')
         
         
-def getDataSensor():
+def getDataArray():
     try:
+        p.stdin.write(bytes('M' + '\n', 'UTF-8'))
         k = int(p.stdin.readline().strip())
         time = []
         coordinate = []
@@ -38,7 +67,7 @@ def init():
 
 
 def checkSyntax(arg):
-        if not (arg in caseOut):
+        if not (arg in Ops):
             raise Exception('not found operation')
           
  
